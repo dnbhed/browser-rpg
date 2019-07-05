5904f13b988f949ff9e79341cac6290196c613a4
Tam O'Donnell <thomashodonnell@gmail.com>
2019-07-05 09:48:00 +0100

    greate react and spring files

---
+++ java-spring/browser-rpg/src/main/java/com/legends/browserrpg/BrowserRpgApplication.java
@@ -0,0 +1,13 @@
+package com.legends.browserrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserRpgApplication.class, args);
+	}
+
+}
