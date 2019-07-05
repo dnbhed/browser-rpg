ded5a861d2b2a214048dd2b15ad9dddfdc077cc2
Ben Hamilton <ben.hamilton@me.com>
2019-07-05 14:03:09 +0100

    add start screen

---
+++ js-react/browser-rpg/src/Components/StartScreenComponents/NewCharacterButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewCharacterButton = () => {
+
+    return (
+        <button>
+            New Character
+        </button>
+    )
+
+
+}
+
+export default NewCharacterButton
---
+++ js-react/browser-rpg/src/Containers/StartScreenContainer.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react'
+import NewCharacterButton from '../Components/StartScreenComponents/NewCharacterButton';
+
+const StartScreenContainer = () => {
+
+    return (
+        <Fragment>
+            <h1>The Game</h1>
+            <NewCharacterButton />
+        </Fragment>
+    ) 
+
+
+
+}
+
+export default StartScreenContainer
