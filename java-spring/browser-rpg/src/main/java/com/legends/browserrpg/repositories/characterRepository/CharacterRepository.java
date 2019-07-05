79aff935bb0d0d27b1dbe33566b624a7c1bb7bfd
Ben Hamilton <ben.hamilton@me.com>
2019-07-05 14:53:54 +0100

    have backend able to communicate with front end without error

--- java-spring/browser-rpg/src/main/java/com/legends/browserrpg/repositories/characterRepository/CharacterRepository.java
+++ java-spring/browser-rpg/src/main/java/com/legends/browserrpg/repositories/characterRepository/CharacterRepository.java
@@ -3,7 +3,9 @@ package com.legends.browserrpg.repositories.characterRepository;
 import com.legends.browserrpg.models.Avatar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Repository
 public interface CharacterRepository extends JpaRepository<Avatar, Long> {
 
