e0343fce2a13bb4d5617f4a29dda09ee05700354
Diego Diniz da Silva <diegodinizz@gmail.com>
2019-07-05 14:47:47 +0100

    NewPlayerContainer added to container in react.

---
+++ js-react/browser-rpg/src/Containers/NewPlayerContainer.js
@@ -0,0 +1,14 @@
+import React ,{Fragment} from 'react'
+
+const NewPlayerContainer = () => {
+
+
+    return (
+       <Fragment>
+           Helloo!!!! New Player!!!
+       </Fragment>
+    )
+
+}
+
+export default NewPlayerContainer;
