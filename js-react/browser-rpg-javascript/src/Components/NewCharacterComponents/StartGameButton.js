2edcf0e0bdb5f8fa98f7374cb8bf46250492f746
Ben Hamilton <ben.hamilton@me.com>
2019-07-08 20:02:37 +0100

    created link to battle screen from create character screen

--- js-react/browser-rpg-javascript/src/Components/NewCharacterComponents/StartGameButton.js
+++ js-react/browser-rpg-javascript/src/Components/NewCharacterComponents/StartGameButton.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const StartGameButton = () => {
 
     return (
-        <h1 hidden>
-            Confirm and Start Game
-        </h1>
+        <h1 id="start-game-button" ><Link to="/battle">FIGHT</Link></h1>
     )
 
 
--- js-react/browser-rpg-javascript/src/Containers/NewCharacterContainer.js
+++ js-react/browser-rpg-javascript/src/Containers/NewCharacterContainer.js
@@ -2,15 +2,15 @@ import React ,{Fragment} from 'react'
 import AvatarDisplay from '../Components/NewCharacterComponents/AvatarDisplay'
 import StartGameButton from '../Components/NewCharacterComponents/StartGameButton'
 
-const NewCharacterContainer = (props) => {
+const NewCharacterContainer = ({spriteID, name, handleClick,handleNameChange,handleSubmit}) => {
 
         return(
         <Fragment>
-            <AvatarDisplay handleClick={props.handleClick}/>
-            <form className="character-form" onSubmit={props.handleSubmit} >
+            <AvatarDisplay handleClick={handleClick}/>
+            <form className="character-form" onSubmit={handleSubmit} >
             <label>Sprite ID</label>
-            <input id="Pog" type="number" value={props.spriteID} readOnly={true}/>
-            <input id= "name" type="text" placeholder="Character name" value ={props.name} onChange={props.handleNameChange}/>
+            <input id="Pog" type="number" value={spriteID} readOnly={true}/>
+            <input id= "name" type="text" placeholder="Character name" value ={name} onChange={handleNameChange}/>
             <input type="submit" value="Create Character"/>
             </form>
             <StartGameButton/>
