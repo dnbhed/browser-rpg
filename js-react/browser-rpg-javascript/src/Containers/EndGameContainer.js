35b66fcc0c005afcab7cc29e78e336453cc642ea
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-07-10 17:17:34 +0100

    Conflict resolved part 2

--- js-react/browser-rpg-javascript/src/Containers/EndGameContainer.js
+++ js-react/browser-rpg-javascript/src/Containers/EndGameContainer.js
@@ -9,7 +9,6 @@ const EndGameContainer = ({ currentCharacter, currentPlayer }) => {
 		return <Redirect to="/" />;
 	}
 
-<<<<<<< HEAD
         
     function printStatus(){
         if (currentCharacter.currentHP === 0 ){
@@ -41,37 +40,6 @@ const EndGameContainer = ({ currentCharacter, currentPlayer }) => {
             <img src={trophy} width="480px" height="480px" className="trophy" />
         </div>
     );
-=======
-	function printStatus() {
-		if (currentCharacter.currentHP === 0) {
-			console.log(currentCharacter.currentHP);
-			return (
-				<Fragment>
-					<h1>You Lose. Try Again!!</h1>
-					<audio src={lose} autoPlay loop={true} />
-				</Fragment>
-			);
-		} else {
-			return (
-				<Fragment>
-					<h1>You Win. Epic Time!</h1>
-					<audio src={victory} autoPlay loop={true} />
-				</Fragment>
-			);
-		}
-	}
-
-	return (
-		<div id="end-game-container">
-			{printStatus()}
-			<h1>Your new score is {currentCharacter.score}</h1>
-			<img src={trophy} width="480px" height="480px" className="trophy" />
-			<Link to="/battle">
-				<h1>Continue?</h1>
-			</Link>
-		</div>
-	);
->>>>>>> develop
 };
 
 export default EndGameContainer;
